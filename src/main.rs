882800909276994dbb31cb28bfab59182a512a26
TragerJoswig-Jones <joswitra@gmail.com>
2022-07-31 14:16:29 -0700

    Add plots and change states to scientific units

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,26 @@ use unifi_gfm::sims::*;
 use unifi_gfm::calc;
 use unifi_gfm::refs::{alpha_beta_fr_polar, alpha_beta_fr_ab};
 use unifi_gfm::constants::PI;
-//use unifi_gfm::dvoc::test;
 
-const OUT_FILE_NAME: &'static str = "images/sample.png";
+const VOLTAGE_FILE_NAME: &'static str = "images/voltage.png";
+const THETA_FILE_NAME: &'static str = "images/thetas.png";
 const POWER_OUT_FILE_NAME: &'static str = "images/powers.png";
+const CURRENT_OUT_FILE_NAME: &'static str = "images/currents.png";
+const DELTA_OUT_FILE_NAME: &'static str = "images/deltas.png";
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     /* 
     DEFINE sys PARAMETERS & CONSTRUCT OBJECTS
     */
-    let v_nom: f32 = 120.;
+    let v_nom: f32 = 80.;
     let f_nom: f32 = 60.;
     let w_nom: f32 = f_nom * 2. * PI;
     let s_rated: f32 = 500.;
     let dt: f32 = 1.0e-4_f32;
     let xi: f32 = 15.;
     let c: f32 = 0.2679;
-    let mut inv = dvoc_controller(v_nom, w_nom, s_rated, dt, xi, c);
-    inv.theta = dt * 0.5;
+    let mut inv = dvoc_controller(v_nom, w_nom, s_rated, xi, c);
+    inv.theta = -dt * 0.475 * w_nom;
+    inv.v = v_nom * 0.999965;
 
     let rf = 0.8;
     let lf = 1.5e-3;
@@ -34,14 +37,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut bus: ACVoltSrc = build_ac_volt_src(v_nom, w_nom, s_rated);
 
     // ODE solver settings
-    let rtol = 1.0e-5; let atol = 1.0e-5;
-    let ode_steps = 5.;
+    let rtol = 1.0e-4; let atol = 1.0e-4;
+    let ode_steps = 20.;
     
     /* 
     RUNNING DYNAMICAL SIMULATION
     */
-    //const n_steps: u32 = 10000;  // TODO: Change this to UPPERCASE or make it a variable
-    let t_end = 0.4;  // Simulate time in seconds
+    let t_end = 0.25;  // Simulate time in seconds
     let n_steps: u32 = (t_end / dt).ceil() as u32;
     let steps: Vec<u32> = (0..n_steps+1).collect();
     let mut t: f32;
@@ -50,8 +52,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut theta_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
     let mut vg_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
     let mut thetag_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
+    let mut delta_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
     let mut p_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
     let mut q_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
+    let mut ia_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
+    let mut ib_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
     'sim: for step in steps {  // TODO: Debug this simulation. Power / Current values seem wonky. Something to do with the per unitization of theta
         t = step as f32 * dt;
         if t > (t_end / 2.) {
@@ -63,10 +68,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         theta_values[step as usize] = (t, inv.theta);
         vg_values[step as usize] = (t, bus.v);
         thetag_values[step as usize] = (t, bus.theta);
+        ia_values[step as usize] = (t, line.i_alpha);
+        ib_values[step as usize] = (t, line.i_beta);
+        let mut delta = inv.theta - bus.theta;
+        if delta > PI {
+            delta = -2.*PI + delta;
+        } else if delta < -PI {
+            delta = 2.*PI + delta;
+        }
+        delta_values[step as usize] = (t, delta);
 
-        inv.step((line.i_alpha, line.i_beta));
+        inv.step(dt, [line.i_alpha, line.i_beta]);
         
-        let v = alpha_beta_fr_polar(inv.kv * inv.v, (inv.w_nom * inv.theta) % (2.*PI));
+        let v = alpha_beta_fr_polar(inv.v, inv.theta);
         let i = alpha_beta_fr_ab(line.i_alpha, line.i_beta);
         (p, q) = calc::calc_power(v, i);
         p_values[step as usize] = (t, p);
@@ -76,7 +90,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Create a new system object and copy over states from existing objects
         let mut ode_line: RLFilter = build_rl_line(w_nom, s_rated, rf, lf);
         let mut ode_bus: ACVoltSrc = build_ac_volt_src(v_nom, w_nom, s_rated);
-        let mut ode_inv = dvoc_controller(v_nom, w_nom, s_rated, dt, xi, c);
+        let mut ode_inv = dvoc_controller(v_nom, w_nom, s_rated, xi, c);
         ode_line.i_alpha = line.i_alpha; ode_line.i_beta = line.i_beta;
         ode_bus.v = bus.v; ode_bus.theta = bus.theta;
         ode_inv.v = inv.v; ode_inv.theta = inv.theta; ode_inv.p_ref = inv.p_ref;
@@ -95,7 +109,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 // let ts = stepper.x_out();
                 let ys = stepper.y_out();
                 let k = ys.len() - 1;
-                bus.v = ys[k][(0, 0)] as f32; bus.theta = ys[k][(1, 0)] as f32;
+                bus.v = ys[k][(0, 0)] as f32; bus.theta = ys[k][(1, 0)] as f32 % (2.*PI);
                 line.i_alpha = ys[k][(2, 0)] as f32; line.i_beta = ys[k][(3, 0)] as f32
             },
             Err(e) => {
@@ -113,9 +127,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         */
 
     }
-    println!("v: {}, theta: {}", inv.v * inv.v_nom, (inv.w_nom * inv.theta) % (2.*PI));
+    println!("v: {}, theta: {}", inv.v, inv.theta);
     println!("ia: {}, ib: {}", line.i_alpha, line.i_beta);
-    println!("vg: {}, thetag: {}", bus.v * bus.v_nom, (bus.w_nom * bus.theta) % (2.*PI));
+    println!("vg: {}, thetag: {}", bus.v, bus.theta);
 
     /* 
     PLOTTING THE RESULTS
@@ -123,18 +137,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let v_values_ = v_values.to_vec();
     let theta_values_ = theta_values.to_vec();
+    let vg_values_ = vg_values.to_vec();
+    let thetag_values_ = thetag_values.to_vec();
+    let delta_values_ = delta_values.to_vec();
     let p_values_ = p_values.to_vec();
     let q_values_ = q_values.to_vec();
+    let ia_values_ = ia_values.to_vec();
+    let ib_values_ = ib_values.to_vec();
 
-    // Plot the data
-    let root = BitMapBackend::new(OUT_FILE_NAME, (640, 480)).into_drawing_area();
+    /* Plot voltage magnitude data */
+    let (_,mut vs): (Vec<f32>, Vec<f32>) = v_values.into_iter().unzip();
+    let (_,mut vgs): (Vec<_>, Vec<_>) = vg_values.into_iter().unzip();
+    vs.append(&mut vgs);
+    let min_v: f32 = vs.iter().fold(f32::INFINITY, |a, &b| a.min(b));
+    let max_v: f32 = vs.iter().fold(0.0f32, |a, &b| a.max(b));
+    let root = BitMapBackend::new(VOLTAGE_FILE_NAME, (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
-        .caption("Voltage & Angle", ("sans-serif", 50).into_font())
+        .caption("Voltage Magnitudes", ("sans-serif", 50).into_font())
         .margin(5 as u32)
         .x_label_area_size(30 as u32)
         .y_label_area_size(30 as u32)
-        .build_cartesian_2d(0.0f32..t_end, -0.1f32..1.5f32)?;
+        .build_cartesian_2d(0.0f32..t_end, min_v..max_v)?;
 
     chart.configure_mesh().draw()?;
 
@@ -143,32 +167,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             v_values_,
             &RED,
         ))?
-        .label("Voltage")
+        .label("Inv Voltage")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    
+    chart
+        .draw_series(LineSeries::new(
+            vg_values_,
+            &GREEN,
+        ))?
+        .label("Bus Voltage")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &GREEN));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    /* Plot voltage angle data */
+    let (_,mut ths): (Vec<f32>, Vec<f32>) = theta_values.into_iter().unzip();
+    let (_,mut thgs): (Vec<_>, Vec<_>) = thetag_values.into_iter().unzip();
+    ths.append(&mut thgs);
+    let min_th: f32 = ths.iter().fold(f32::INFINITY, |a, &b| a.min(b));
+    let max_th: f32 = ths.iter().fold(0.0f32, |a, &b| a.max(b));
+    let root = BitMapBackend::new(THETA_FILE_NAME, (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Voltage Angles", ("sans-serif", 50).into_font())
+        .margin(5 as u32)
+        .x_label_area_size(30 as u32)
+        .y_label_area_size(30 as u32)
+        .build_cartesian_2d(0.0f32..t_end, min_th..max_th)?;
+
+    chart.configure_mesh().draw()?;
 
     chart
         .draw_series(LineSeries::new(
             theta_values_,
             &BLUE,
         ))?
-        .label("Angle")
+        .label("Inv Theta")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
-    
-    chart
-        .draw_series(LineSeries::new(
-            vg_values,
-            &GREEN,
-        ))?
-        .label("Grid Voltage")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &GREEN));
 
     chart
         .draw_series(LineSeries::new(
-            thetag_values,
+            thetag_values_,
             &BLACK,
         ))?
-        .label("Grid Angle")
+        .label("Bus Theta")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLACK));
+
     chart
         .configure_series_labels()
         .background_style(&WHITE.mix(0.8))
@@ -220,6 +270,81 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     root.present()?;
 
+    /* Plot line current data */
+    // Find the maximum and minimum values of ia and ib for plotting ylims
+    let (_,mut ias): (Vec<f32>, Vec<f32>) = ia_values.into_iter().unzip();
+    let (_,mut ibs): (Vec<_>, Vec<_>) = ib_values.into_iter().unzip();
+    ias.append(&mut ibs);
+    let min_i: f32 = ias.iter().fold(0.0f32, |a, &b| a.min(b));
+    let max_i: f32 = ias.iter().fold(0.0f32, |a, &b| a.max(b));
+    let root = BitMapBackend::new(CURRENT_OUT_FILE_NAME, (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Line Currents", ("sans-serif", 50).into_font())
+        .margin(5 as u32)
+        .x_label_area_size(30 as u32)
+        .y_label_area_size(30 as u32)
+        .build_cartesian_2d(0.0f32..t_end, (min_i-0.1)..(max_i+0.1))?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            ia_values_,
+            &RED,
+        ))?
+        .label("ia")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    chart
+        .draw_series(LineSeries::new(
+            ib_values_,
+            &BLUE,
+        ))?
+        .label("ib")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+
+    /* Plot voltage angle difference data */
+    // Find the maximum and minimum values of delta for plotting ylims
+    let (_, deltas): (Vec<f32>, Vec<f32>) = delta_values.into_iter().unzip();
+    let min_d: f32 = deltas.iter().fold(0.0f32, |a, &b| a.min(b));
+    let max_d: f32 = deltas.iter().fold(0.0f32, |a, &b| a.max(b));
+    let root = BitMapBackend::new(DELTA_OUT_FILE_NAME, (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Voltage Angle Difference", ("sans-serif", 50).into_font())
+        .margin(5 as u32)
+        .x_label_area_size(30 as u32)
+        .y_label_area_size(30 as u32)
+        .build_cartesian_2d(0.0f32..t_end, (min_d)..(max_d))?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            delta_values_,
+            &RED,
+        ))?
+        .label("delta")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
     Ok(())
 }
 
@@ -237,10 +362,10 @@ impl ode_solvers::System<State> for DigitalInverterSMIB {
     fn system(&self, _t: f64, y: &State, dy: &mut State) {
         // y: [vg, thetag, ia, ib]
         let (dvg_dt, dthetag_dt) = self.bus.dynamics([y[0], y[1]], []);
-        let (di_alpha_dt, di_beta_dt) = self.line.dynamics([y[2], y[3]], [self.inv.kv * self.inv.v, (self.inv.w_nom * self.inv.theta) % (2.*PI), 
-                                                                                         self.bus.v_nom * y[0], (self.bus.w_nom * y[1]) % (2.*PI)]);
-        dy[0] = 0.; // dvg_dt;
-        dy[1] = 1.; // dthetag_dt;
+        let (di_alpha_dt, di_beta_dt) = self.line.dynamics([y[2], y[3]], [self.inv.v, self.inv.theta, 
+                                                                                         y[0], y[1]]);
+        dy[0] = dvg_dt;
+        dy[1] = dthetag_dt;
         dy[2] = di_alpha_dt;
         dy[3] = di_beta_dt;
     }
