0ef84cbf591fb4f984749e6f9423fa2807b3724d
TragerJoswig-Jones <joswitra@gmail.com>
2022-07-31 13:33:58 -0700

    Update simulation to have states in scientific units

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use unifi_gfm::sims::*;
 use unifi_gfm::calc;
 use unifi_gfm::refs::{alpha_beta_fr_polar, alpha_beta_fr_ab};
 use unifi_gfm::constants::PI;
-//use unifi_gfm::inv::test;
 
-const OUT_FILE_NAME: &'static str = "images/voltages.png";
+const VOLTAGE_FILE_NAME: &'static str = "images/voltage.png";
+const THETA_FILE_NAME: &'static str = "images/thetas.png";
 const POWER_OUT_FILE_NAME: &'static str = "images/powers.png";
 const CURRENT_OUT_FILE_NAME: &'static str = "images/currents.png";
 const DELTA_OUT_FILE_NAME: &'static str = "images/deltas.png";
@@ -17,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     /* 
     DEFINE SYSTEM PARAMETERS & CONSTRUCT OBJECTS
     */
-    let v_nom: f32 = 120.;
+    let v_nom: f32 = 80.;
     let f_nom: f32 = 60.;
     let w_nom: f32 = f_nom * 2. * PI;
     let s_rated: f32 = 500.;
@@ -25,21 +25,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let xi: f32 = 15.;
     let c: f32 = 0.2679;
     let mut inv = build_dvoc_controller(v_nom, w_nom, s_rated, xi, c);
-    inv.theta = -dt * 0.5;  // Initialize inverter angle leading the grid angle
+    inv.theta = -dt * 0.47 * w_nom;  // Initialize inverter angle leading the grid angle by ~half a cycle to start closer to the digital equalibria
+    inv.v = v_nom * 0.999965;  // Initialize inverter voltage slightly lower than nominal to start closer to the digital equalibria
 
     let rf = 0.8;
     let lf = 1.5e-3;
     let mut line: RLFilter = build_rl_line(w_nom, s_rated, rf, lf);
     let mut bus: ACVoltSrc = build_ac_volt_src(v_nom, w_nom, s_rated);
 
-    // TODO: Debug simulation results being weird for cont_n_steps > 5. Seems to be something happening in the grid or line dynamics every some period
-
-    /* 
+    /*
     RUNNING DYNAMICAL SIMULATION
     */
-    let t_end = 0.2;  // Simulate time in seconds
+    let t_end = 0.5;  // Simulate time in seconds
     let n_steps: u32 = (t_end / dt).ceil() as u32;
-    let cont_n_steps = 50;  // Number of steps taken for 'continuous' dynamics for each digital step
+    let cont_n_steps = 20;  // Number of steps taken for 'continuous' dynamics for each digital step
     let cont_dt = dt / (cont_n_steps as f32);
     let steps: Vec<u32> = (0..n_steps+1).collect();
     let mut t: f32;
@@ -55,10 +54,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut ib_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
     for step in steps {
         t = step as f32 * dt;
-        if t > (t_end / 4.) {
-            inv.set_p_ref(0.0);
-        }
-        
         if t > (t_end / 2.) {
             inv.set_p_ref(100.0);
         }
@@ -70,7 +65,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         thetag_values[step as usize] = (t, bus.theta);
         ia_values[step as usize] = (t, line.i_alpha);
         ib_values[step as usize] = (t, line.i_beta);
-        let delta = inv.theta - bus.theta;
+        let mut delta = inv.theta - bus.theta;
+        if delta > PI {
+            delta = -2.*PI + delta;
+        } else if delta < -PI {
+            delta = 2.*PI + delta;
+        }
         delta_values[step as usize] = (t, delta);
 
         // Step the controller
@@ -79,19 +79,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Step the system
         for n in 0..cont_n_steps {
             // Calculate power
-            let v = alpha_beta_fr_polar(inv.kv * inv.v, (inv.w_nom * inv.theta) % (2.*PI));
+            let v = alpha_beta_fr_polar(inv.v, inv.theta);
             let i = alpha_beta_fr_ab(line.i_alpha, line.i_beta);
             (p, q) = calc::calc_power(v, i);
             p_values[(cont_n_steps*step + n) as usize] = (t + (n as f32)*cont_dt, p);
             q_values[(cont_n_steps*step + n) as usize] = (t + (n as f32)*cont_dt, q);
             bus.step_(cont_dt);
-            line.step(cont_dt, [inv.kv * inv.v, (inv.w_nom * inv.theta) % (2.*PI), 
-                           bus.v_nom * bus.v, (bus.w_nom * bus.theta) % (2.*PI)]);
+            line.step(cont_dt, [inv.v, inv.theta, 
+                                   bus.v, bus.theta]);
         }
     }
-    println!("v: {}, theta: {}", inv.v * inv.v_nom, (inv.w_nom * inv.theta) % (2.*PI));
+    println!("v: {}, theta: {}", inv.v, inv.theta);
     println!("ia: {}, ib: {}", line.i_alpha, line.i_beta);
-    println!("vg: {}, thetag: {}", bus.v * bus.v_nom, (bus.w_nom * bus.theta) % (2.*PI));
+    println!("vg: {}, thetag: {}", bus.v, bus.theta);
     println!("p: {}, q: {}", p, q);
 
     /* 
@@ -100,21 +100,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let v_values_ = v_values.to_vec();
     let theta_values_ = theta_values.to_vec();
+    let vg_values_ = vg_values.to_vec();
+    let thetag_values_ = thetag_values.to_vec();
     let delta_values_ = delta_values.to_vec();
     let p_values_ = p_values.to_vec();
     let q_values_ = q_values.to_vec();
     let ia_values_ = ia_values.to_vec();
     let ib_values_ = ib_values.to_vec();
 
-    // Plot the data
-    let root = BitMapBackend::new(OUT_FILE_NAME, (640, 480)).into_drawing_area();
+    /* Plot voltage magnitude data */
+    let (_,mut vs): (Vec<f32>, Vec<f32>) = v_values.into_iter().unzip();
+    let (_,mut vgs): (Vec<_>, Vec<_>) = vg_values.into_iter().unzip();
+    vs.append(&mut vgs);
+    let min_v: f32 = vs.iter().fold(f32::INFINITY, |a, &b| a.min(b));
+    let max_v: f32 = vs.iter().fold(0.0f32, |a, &b| a.max(b));
+    let root = BitMapBackend::new(VOLTAGE_FILE_NAME, (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
-        .caption("Voltage & Angle", ("sans-serif", 50).into_font())
+        .caption("Voltage Magnitudes", ("sans-serif", 50).into_font())
         .margin(5 as u32)
         .x_label_area_size(30 as u32)
         .y_label_area_size(30 as u32)
-        .build_cartesian_2d(0.0f32..t_end, -0.1f32..1.1f32)?;
+        .build_cartesian_2d(0.0f32..t_end, min_v..max_v)?;
 
     chart.configure_mesh().draw()?;
 
@@ -123,32 +130,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             v_values_,
             &RED,
         ))?
-        .label("Voltage")
+        .label("Inv Voltage")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    
+    chart
+        .draw_series(LineSeries::new(
+            vg_values_,
+            &GREEN,
+        ))?
+        .label("Bus Voltage")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &GREEN));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    /* Plot voltage angle data */
+    let (_,mut ths): (Vec<f32>, Vec<f32>) = theta_values.into_iter().unzip();
+    let (_,mut thgs): (Vec<_>, Vec<_>) = thetag_values.into_iter().unzip();
+    ths.append(&mut thgs);
+    let min_th: f32 = ths.iter().fold(f32::INFINITY, |a, &b| a.min(b));
+    let max_th: f32 = ths.iter().fold(0.0f32, |a, &b| a.max(b));
+    let root = BitMapBackend::new(THETA_FILE_NAME, (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Voltage Angles", ("sans-serif", 50).into_font())
+        .margin(5 as u32)
+        .x_label_area_size(30 as u32)
+        .y_label_area_size(30 as u32)
+        .build_cartesian_2d(0.0f32..t_end, min_th..max_th)?;
+
+    chart.configure_mesh().draw()?;
 
     chart
         .draw_series(LineSeries::new(
             theta_values_,
             &BLUE,
         ))?
-        .label("Angle")
+        .label("Inv Theta")
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
-    
-    chart
-        .draw_series(LineSeries::new(
-            vg_values,
-            &GREEN,
-        ))?
-        .label("bus Voltage")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
 
     chart
         .draw_series(LineSeries::new(
-            thetag_values,
+            thetag_values_,
             &BLACK,
         ))?
-        .label("bus Angle")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
+        .label("Bus Theta")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLACK));
+
     chart
         .configure_series_labels()
         .background_style(&WHITE.mix(0.8))
