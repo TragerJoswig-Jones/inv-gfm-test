47cff99d19f5094d8b6b4d7646d07746f9b54693
TragerJoswig-Jones <joswitra@gmail.com>
2022-08-19 15:46:07 -0700

    Test dVOC LCL filter sim and add double-loop voltage sim

--- src/main.rs
+++ src/main.rs
@@ -26,17 +26,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let c: f32 = 0.2679;
     let gamma: f32 = 1.; 
 
+    let rf = 0.4;  // filter-side resistance
+    let lf = 1.5e-3;  // filter-side inductance
+    let cf = 10e-6;  // filter capacitance
+    let rc = 0.05;  // filter capacitor parasitic resistance
+    let rg = 0.4;  // grid-side resistance
+    let lg = 1.5e-3;  // grid-side inductance
+    let rv = 0;  // virtual impedance
+    let zf = libm::sqrtf(rf*rf+(lf*w_nom)*(lf*w_nom));  // filter nominal inductance
+
+    let w_cur = 2.*PI*5000.;
+    let w_vol = 2.*PI*800.;
+    
+    let kp_v = 2.*w_vol*cf;
+    let ki_v = 2.*kp_v*w_vol*w_vol/w_cur;
+    let kp_i = lf*w_cur;
+    let ki_i = rf*w_cur;
+
     let z_base = 3. * v_nom * v_nom / s_rated;
 
     let mut inv = build_dvoc_controller(v_nom, w_nom, xi, c);
-    inv.x[(1)] = 0.003;  // Initialize inverter angle to be off from the grid
-    inv.x[(0)] = 1.1;  // Initialize inverter voltage
+    inv.x[(1)] = 0.003;  // Initialize inverter angle to be off from the grid to test presync
+    inv.x[(0)] = 1.1;  // Initialize inverter voltage to be off from v_nom to test presync
     let mut gfm = build_gfm(&mut inv, gamma);  // Place the dVOC controller within a GFM interface object
+    let mut voltage_loop = build_double_loop_voltage_controller(v_nom, kp_v, ki_v, kp_i, ki_i, lf, cf);
 
-    let rf = 0.8;
-    let lf = 1.5e-3;
-    let mut line: RLFilter<f32> = build_rl_line(f_nom, rf / z_base, lf / z_base);
-    let mut bus: ACVoltSrc<f32> = build_ac_volt_src(v_nom, w_nom);
+    let mut line: LclFilter<f32> = build_lcl_filter(w_nom, v_nom, rf / z_base, lf / z_base, rc / z_base, cf / z_base, rg / z_base, lg / z_base);
+    let mut bus: AcVoltSrc<f32> = build_ac_volt_src(v_nom, w_nom);
 
     /*
     RUNNING DYNAMICAL SIMULATION
@@ -55,6 +71,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut i_alpha_sample: f32; let mut i_beta_sample: f32;
     let mut v_grid_sample: f32; let mut theta_grid_sample: f32;
     let mut v_grid: f32; let mut theta_grid: f32; let mut v_inv: [f32; 2];
+    let mut v_to_alpha_beta: AlphaBeta<f32>;
     // Simulation data vectors
     let mut v_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
     let mut theta_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
@@ -65,7 +82,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut q_values: Vec<(f32, f32)> = vec![(0., 0.); ((n_steps+1)*cont_n_steps) as usize];
     let mut ia_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
     let mut ib_values: Vec<(f32, f32)> = vec![(0., 0.); (n_steps+1) as usize];
-    for step in steps {  // TODO: Debug this to see where the overflow occurs...
+    for step in steps {
         t = step as f32 * dt;
         if t > t_step {
             gfm.set_p_ref(1.0);
@@ -90,16 +107,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Sample the current
         i_alpha_sample = line.x[(0)];
         i_beta_sample = line.x[(1)];
+        // Sample the grid
         v_grid_sample = bus.x[(0)];
         theta_grid_sample = bus.x[(1)] * bus.w_nom;
-        let v_grid_alpha_beta = AlphaBeta::from_polar(v_grid_sample, theta_grid_sample);
+        let v_grid_sample_alpha_beta = AlphaBeta::from_polar(v_grid_sample, theta_grid_sample);
+        // Get alpha-beta gfm voltage
+        let v_inv_alpha_beta = AlphaBeta::from_polar(v_inv[0], v_inv[1] * gfm.ctrl.get_w_nom());
 
         // Step the system
         for n in 0..cont_n_steps {
             // Calculate power
-            let v = AlphaBeta::from_polar(v_inv[0], v_inv[1] * gfm.ctrl.get_w_nom());
             let i = AlphaBeta::from_ab_(line.x[(0)], line.x[(1)]);
-            (p, q) = calc_ab_power(v, i);
+            (p, q) = calc_ab_power(&v_inv_alpha_beta, &i);
             p_values[(cont_n_steps*step + n) as usize] = (t + (n as f32)*cont_dt, p);
             q_values[(cont_n_steps*step + n) as usize] = (t + (n as f32)*cont_dt, q);
             
@@ -108,17 +127,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 gfm.disable_presync();
                 v_grid = bus.x[(0)];  // TODO: rename this for clarity
                 theta_grid = bus.x[(1)] * bus.w_nom;
+                v_to_alpha_beta = AlphaBeta::from_polar(v_grid, theta_grid);
             } else {
-                v_grid = v_inv[0];
-                theta_grid = v_inv[1] * gfm.ctrl.get_w_nom();
+                v_to_alpha_beta = AlphaBeta::from_polar(v_inv[0], v_inv[1] * gfm.ctrl.get_w_nom());
             }
-            line.step(cont_dt, [v_inv[0], v_inv[1] * gfm.ctrl.get_w_nom(), 
-                                   v_grid, theta_grid]);
+            line.step(cont_dt, [v_inv_alpha_beta.alpha, v_inv_alpha_beta.beta, 
+                                   v_to_alpha_beta.alpha, v_to_alpha_beta.beta]);
             bus.step_(cont_dt);
         }
 
         // Step the controller after a z^-1 delay
-        gfm.gfm_step(dt, [i_alpha_sample, i_beta_sample], [v_grid_alpha_beta.alpha, v_grid_alpha_beta.beta]);
+        gfm.gfm_step(dt, [i_alpha_sample, i_beta_sample], [v_grid_sample_alpha_beta.alpha, v_grid_sample_alpha_beta.beta]);
     }
     println!("v: {}, theta: {}", inv.x[(0)], inv.x[(1)]);
     println!("ia: {}, ib: {}", line.x[(0)], line.x[(1)]);
